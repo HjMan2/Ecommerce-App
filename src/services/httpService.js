eebbedd522db73968b6b012c5cce79f8d3d4df67
HjMan2 <79367122+HjMan2@users.noreply.github.com>
2021-06-30 13:39:38 +0430

    create request module with axios

---
+++ src/services/httpService.js
@@ -0,0 +1,34 @@
+import axios from "axios";
+import { toast } from "react-toastify";
+
+axios.interceptors.request.use((response) => {
+  // console.log("SHOW LOADING...");
+  return response;
+}, null);
+
+axios.interceptors.response.use(
+  (config) => {
+    // console.log("HIDE LOADING...");
+    return config;
+  },
+  (error) => {
+    const expectedError =
+      error.response &&
+      error.response.status >= 400 &&
+      error.response.status < 500;
+    if (!expectedError) {
+      toast.error("an unexpected error occurred!");
+      console.log("Error message: ", error);
+    }
+    return Promise.reject(error);
+  }
+);
+
+const http = {
+  get: axios.get,
+  post: axios.post,
+  delete: axios.delete,
+  put: axios.put,
+};
+
+export default http;

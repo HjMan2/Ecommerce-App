c1e46ebb63621595fb1f6ee13f25a81d35fe6898
HjMan2 <79367122+HjMan2@users.noreply.github.com>
2021-06-30 13:59:36 +0430

    complete table component for showing data in three admin page

--- src/common/Components/Table.js
+++ src/common/Components/Table.js
@@ -1,9 +1,73 @@
-import { Table } from 'react-bootstrap'
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
 
-function Table() {
-    return (
-        <Table striped bordered hover></Table>
-    )
+const useStyles = makeStyles({
+  table: {
+    minWidth: 650,
+  },
+  theadCell: {
+    fontSize: "1rem",
+    fontWeight: "600",
+  },
+});
+
+const formatDate = (date) => {
+  return new Date(+date).toLocaleDateString("fa-IR");
+};
+
+const createKey = (item, column) => {
+  return item.id + (column.path || column.key);
+};
+const renderCell = (item, column) => {
+  if (column.key) return column.details(item);
+  if (column.path === "timeStamp") return formatDate(item[column.path]);
+  return item[column.path];
+};
+
+function TableData({ columns, data }) {
+  const classes = useStyles();
+
+  return (
+    <TableContainer component={Paper}>
+      <Table className={classes.table} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            {columns.map((item) => {
+              return (
+                <TableCell
+                  className={classes.theadCell}
+                  key={item.label}
+                  align="right"
+                >
+                  {item.label}
+                </TableCell>
+              );
+            })}
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {data.map((item) => (
+            <TableRow key={item.id}>
+              {columns.map((column) => {
+                return (
+                  <TableCell align="right" key={createKey(item, column)}>
+                    {renderCell(item, column)}
+                  </TableCell>
+                );
+              })}
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
 }
 
-export { Table }
+export { TableData };

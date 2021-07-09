cd8607de781a5a3d70f8d34ff4a46bcde2141004
HjMan2 <79367122+HjMan2@users.noreply.github.com>
2021-07-09 11:54:04 +0430

    create product list and product page

--- src/pages/Components/Product/Product.js
+++ src/pages/Components/Product/Product.js
@@ -1,12 +1,96 @@
-import { Link } from 'react-router-dom'
+import { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import { getProduct } from "../../../services/ordersService";
+import { transformToHtml } from "../../../utils/transformToHtml";
+import { makeStyles } from "@material-ui/core/styles";
+import { Button } from "react-bootstrap";
+import { TextField, Typography, Breadcrumbs } from "@material-ui/core";
+import NavigateBeforeIcon from "@material-ui/icons/NavigateBefore";
+
+const useStyles = makeStyles((theme) => ({
+  product: {
+    display: "flex",
+    flexDirection: "column",
+    flexGrow: 1,
+    margin: `${theme.spacing(2)}px 0`,
+    gap: theme.spacing(10),
+  },
+  general: {
+    display: "flex",
+    gap: theme.spacing(5),
+    "& > img": {
+      width: "300px",
+      height: "300px",
+      objectFit: "cover",
+    },
+  },
+  detalis: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-between",
+  },
+  description: {},
+  addBtn: {
+    backgroundColor: "#198754",
+    color: "white",
+  },
+  fileds: {
+    display: "flex",
+    gap: theme.spacing(2),
+    alignItems: "center",
+  },
+  bold: {
+    fontSize: "1.875rem",
+    fontWeight: "600",
+  },
+}));
+
 function Product() {
-    return (
-        <>
-        
-        <h1>شلوار</h1>
-        <Link to="/cart">افزودن به سبد خرید</Link>
-        </>
-    )
+  const classes = useStyles();
+  const { productId } = useParams();
+  const [product, setProduct] = useState({});
+
+  useEffect(() => {
+    getProduct(productId).then((res) => setProduct(res));
+  }, [productId]);
+
+  return (
+    <>
+      <div className={classes.product}>
+        <div className={classes.general}>
+          <img src={product.image} alt={product.name} />
+          <div className={classes.detalis}>
+            <div className={classes.bold}>{product.name}</div>
+            <div>
+              <Breadcrumbs separator={<NavigateBeforeIcon fontSize="large" />}>
+                <Typography className={classes.bold}>{product.category}</Typography>
+                {product.pathName && product.pathName.split(",").map((item) => (
+                  <Typography key={item} className={classes.bold}>{item}</Typography>
+                ))}
+              </Breadcrumbs>
+            </div>
+            <div className={classes.bold}>{product.price} تومان</div>
+            <div className={classes.fileds}>
+              <TextField
+                id="outlined-number"
+                type="number"
+                InputLabelProps={{
+                  shrink: true,
+                }}
+                variant="outlined"
+              />
+              <Button variant="contained" className={classes.addBtn} size="lg">
+                افزودن به سبد خرید
+              </Button>
+            </div>
+          </div>
+        </div>
+        <div className={classes.description}>
+          {transformToHtml(product.description)}
+        </div>
+      </div>
+    </>
+  );
 }
 
-export { Product }
+export { Product };
--- src/pages/Components/Products/Products.js
+++ src/pages/Components/Products/Products.js
@@ -1,12 +1,107 @@
-import { ProductsList } from '../../../common/Components/ProductsList'
+import { useEffect, useState, Fragment } from "react";
+import { useParams, Link, useLocation } from "react-router-dom";
+import { getAllProducts, getCategories } from "../../../services/ordersService";
+import { ProductsList } from "../../../common/Components/ProductsList";
+import { Grid } from "@material-ui/core";
+import { Paper } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import { Paginate } from "../../../common/Components/Paginate";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: "2",
+    margin: `${theme.spacing(2)}px 0`,
+  },
+  parent: {
+    display: "flex",
+  },
+  list: {
+    flexGrow: "1",
+  },
+  paper: {
+    padding: theme.spacing(2),
+    color: theme.palette.text.secondary,
+  },
+}));
 
 function Products() {
-    return(
-        <>
-            <h1>صفحه محصول</h1>
-            <ProductsList/>
-        </>
-    )
+  const classes = useStyles();
+  const [products, setProducts] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const search = useLocation().search;
+  const pageNumber = new URLSearchParams(search).get("page") || 1;
+  const status = new URLSearchParams(search).get("status");
+  const { activeTab } = useParams();
+  useEffect(() => {
+    getAllProducts().then((response) => {
+      setProducts(response);
+    });
+    getCategories().then((res) => setCategories(res));
+  }, []);
+
+  const categoryList = () => {
+    if (status) {
+      const list = products.filter(
+        (item) => item.pathName.split(",")[1] === status
+      );
+      return list;
+    }
+    const list = products.filter(
+      (item) => item.pathName.split(",")[0] === activeTab
+    );
+    return list;
+  };
+
+  const slicedData = () => {
+    const list = categoryList().slice(pageNumber * 10 - 10, pageNumber * 10);
+    return list;
+  };
+
+  return (
+    <>
+      <div className={classes.root}>
+        <Grid container spacing={3}>
+          <Grid item lg={2}>
+            <Paper className={classes.paper}>
+              <ul>
+                {categories.map((cat, index) => (
+                  <li key={index}>
+                    <strong>{cat.name}</strong>
+                    <ul>
+                      {cat.subCats.map((subCat, index) => (
+                        <Fragment key={index}>
+                          <li>
+                            <Link to={`/search/${subCat.subName}`}>
+                              {subCat.subName}
+                            </Link>
+                          </li>
+                          <ul>
+                            {subCat.children.map((child) => (
+                              <li key={child}>
+                                <Link
+                                  to={`/search/${subCat.subName}?status=${child}`}
+                                >
+                                  {child}
+                                </Link>
+                              </li>
+                            ))}
+                          </ul>
+                        </Fragment>
+                      ))}
+                    </ul>
+                  </li>
+                ))}
+              </ul>
+            </Paper>
+          </Grid>
+          <Grid item lg={10} xs={12} container spacing={3}>
+            <ProductsList list={slicedData()} />
+          </Grid>
+        </Grid>
+      </div>
+      <Paginate count={Math.ceil(categoryList().length / 10)} path="search" />
+    </>
+  );
 }
 
-export { Products }
+export { Products };

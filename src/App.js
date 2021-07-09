e85eb3d7afc41799790a73fa35024c5b4207a419
HjMan2 <79367122+HjMan2@users.noreply.github.com>
2021-07-09 11:48:23 +0430

    add toast for notification

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import AppRoute from "./route/App.route";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 function App() {
   return (
-    <AppRoute/>
+    <>
+      <ToastContainer />
+      <AppRoute />
+    </>
   );
 }
 

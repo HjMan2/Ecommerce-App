f146398ad6bac36e363ede3238bdb003e0785434
HjMan2 <79367122+HjMan2@users.noreply.github.com>
2021-07-09 11:53:36 +0430

    create home page with admin special products

--- src/pages/Components/Home/Components/AdminSpecialproducts.js
+++ src/pages/Components/Home/Components/AdminSpecialproducts.js
@@ -1,7 +1,35 @@
-import { ProductsList } from '../../../../common/Components/ProductsList'
+import { Fragment } from "react";
+import { ProductCard } from "../../../../common/Components/ProductCard";
+import { Grid } from "@material-ui/core";
+import { Link } from "react-router-dom";
 
-function AdminSpecialProducts() {
-    return <ProductsList/>
+function AdminSpecialProducts(props) {
+  const { categories, products } = props;
+  return (
+    <>
+      {categories.map((cat) => {
+        return cat.subCats.map((subCat, index) => {
+          const filteredList = products.filter((item) => {
+            return item.pathName.split(",")[0] === subCat.subName;
+          });
+          return (
+            <Fragment key={cat.id + subCat.subName + index}>
+              <Grid item xs={12}>
+                <Link to={`/search/${subCat.subName}`}>{subCat.subName}</Link>
+              </Grid>
+              {filteredList.map((item, index) => {
+                return index < 6 ? (
+                  <Grid item lg={4} xs={12} key={item.id}>
+                    <ProductCard item={item} />
+                  </Grid>
+                ) : null;
+              })}
+            </Fragment>
+          );
+        });
+      })}
+    </>
+  );
 }
 
-export { AdminSpecialProducts }
+export { AdminSpecialProducts };
--- src/pages/Components/Home/Home.js
+++ src/pages/Components/Home/Home.js
@@ -1,14 +1,33 @@
-import { Link } from 'react-router-dom'
-import { AdminSpecialProducts } from './Components/AdminSpecialproducts'
+import { useEffect, useState } from "react";
+import { getAllProducts, getCategories } from "../../../services/ordersService";
+import { AdminSpecialProducts } from "./Components/AdminSpecialproducts";
+import Grid from "@material-ui/core/Grid";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: "1",
+    margin: `${theme.spacing(2)}px 0`,
+  },
+}));
+
 function Home() {
-    return (
-        <>
-            <h1>Home Page.</h1>
-            <AdminSpecialProducts/>
-            <Link to="/goods/poshk">پوشاک</Link>
-        </>
-        
-    )
+  const classes = useStyles();
+  const [products, setProducts] = useState([]);
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    getAllProducts().then((res) => setProducts(res));
+    getCategories().then((res) => setCategories(res));
+  }, []);
+
+  return (
+    <div className={classes.root}>
+      <Grid container spacing={3}>
+        <AdminSpecialProducts categories={categories} products={products}/>
+      </Grid>
+    </div>
+  );
 }
 
-export { Home }
+export { Home };

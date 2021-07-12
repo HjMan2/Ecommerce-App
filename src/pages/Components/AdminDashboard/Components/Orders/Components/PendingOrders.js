b7b273d79aa81bf9db81041d7998c63a7d69a19e
HjMan2 <79367122+HjMan2@users.noreply.github.com>
2021-07-12 20:06:46 +0430

    add order details to its parent and handle events

--- src/pages/Components/AdminDashboard/Components/Orders/Components/PendingOrders.js
+++ src/pages/Components/AdminDashboard/Components/Orders/Components/PendingOrders.js
@@ -1,10 +1,11 @@
 import { useState, useEffect } from "react";
 import { useLocation } from "react-router";
-import { getOrders } from "../../../../../../services/ordersService";
+import { getOrders, saveToDeliveredOrders } from "../../../../../../services/ordersService";
 import { TableData } from "../../../../../../common/Components/Table";
 import { Paginate } from "../../../../../../common/Components/Paginate";
 import Button from "@material-ui/core/Button";
-function PendingOrders() {
+import { OrderDetails } from "./Components/OrdersDetails";
+function PendingOrders({ onOpen, onClose, open, customer }) {
   const [result, setResult] = useState([]);
   const [responseLength, setResponseLength] = useState(5);
   const search = useLocation().search;
@@ -30,15 +31,35 @@ function PendingOrders() {
       label: "زمان‌ ثبت سفارش",
     },
     {
-      details: (person) => <Button variant="contained">بررسی‌سفارش</Button>,
+      details: (person) => (
+        <Button variant="contained" onClick={() => onOpen(person)}>
+          بررسی‌سفارش
+        </Button>
+      ),
       key: "details",
       label: "جزئیات",
     },
   ];
+
+  const saveToDelivered = (item) => {
+    const id = item.id
+    const status = { status: "delivered"}
+    saveToDeliveredOrders(id, status)
+    const newResult = result.filter(customer => customer.id !== id)
+    setResult(newResult)
+    onClose()
+  }
+
   return (
     <>
       <TableData columns={columns} data={result} />
-      <Paginate count={Math.ceil(responseLength / 5)} path="admin-dashboard"/>
+      <Paginate count={Math.ceil(responseLength / 5)} path="admin-dashboard" />
+      <OrderDetails
+        customer={customer}
+        onClose={onClose}
+        open={open}
+        saveToDelivered={saveToDelivered}
+      />
     </>
   );
 }

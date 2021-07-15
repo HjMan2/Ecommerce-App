583548277aa2337700ebfbd138c6c6c1cecc63bf
HjMan2 <79367122+HjMan2@users.noreply.github.com>
2021-07-15 23:48:02 +0430

    type for redux actions and reducers

---
+++ src/redux/cart/cartTypes.js
@@ -0,0 +1,5 @@
+export const ADD_TO_CART = "ADD_TO_CART";
+export const DELETE_FROM_CART = "DELETE_FROM_CART";
+export const CLEAR_CART = "CLEAR_CART";
+export const REFILL_CART = "REFILL_CART";
+export const SET_TOTAL_PRICE = "SET_TOTAL_PRICE";

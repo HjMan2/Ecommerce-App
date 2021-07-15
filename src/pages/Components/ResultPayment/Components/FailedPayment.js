21068c5d5299a88f900a0e34ca1ad21b0104947c
HjMan2 <79367122+HjMan2@users.noreply.github.com>
2021-07-15 23:59:16 +0430

    failed payment page and success with style

--- src/pages/Components/ResultPayment/Components/FailedPayment.js
+++ src/pages/Components/ResultPayment/Components/FailedPayment.js
@@ -1,5 +1,21 @@
+import { useDispatch } from "react-redux";
+import { refillCart } from "../../../../redux/cart/cartActions";
+import CancelIcon from "@material-ui/icons/Cancel";
+import styles from "./resultPage.module.css";
+
 function FailedPayment() {
-    return
+  const products = JSON.parse(localStorage.getItem("customerDetails"))[0].products
+  console.log(products)
+  const dispatch = useDispatch();
+  dispatch(refillCart(products));
+  return (
+    <div className={styles.message}>
+      <CancelIcon style={{ color: "#CF2A27", fontSize: 100 }} />
+      <p className={styles.margin}>
+        پرداخت موقفیت آمیز نبود سفارش شما در انتظار پرداخت است.
+      </p>
+    </div>
+  );
 }
 
-export { FailedPayment }
+export { FailedPayment };
--- src/pages/Components/ResultPayment/Components/SuccessPayment.js
+++ src/pages/Components/ResultPayment/Components/SuccessPayment.js
@@ -1,5 +1,36 @@
+import {
+  saveToPendingList,
+  setCountInStock,
+} from "../../../../services/ordersService";
+import CheckCircleIcon from "@material-ui/icons/CheckCircle";
+import styles from "./resultPage.module.css";
+import { useEffect } from "react";
 function SuccessPayment() {
-    return 
+  const componentDidMount = () => {
+    const customer = JSON.parse(localStorage.getItem("customerDetails"))[0];
+    saveToPendingList(customer);
+    const stockList = customer.products.map((item) => ({
+      id: item.id,
+      numberInStock: item.numberInStock,
+      numberOfDemand: item.numberOfDemand,
+    }));
+    setCountInStock(stockList);
+  };
+
+  useEffect(() => {
+    componentDidMount();
+  }, []);
+
+  return (
+    <div className={styles.message}>
+      <CheckCircleIcon style={{ color: "#009E0F", fontSize: 100 }} />
+
+      <p className={styles.margin}>
+        با تشکر از پرداخت شما سفارش شما ثبت شد و جهت هماهنگی ارسال با شما تماس
+        گرفته خواهد شد.
+      </p>
+    </div>
+  );
 }
 
-export { SuccessPayment }
+export { SuccessPayment };

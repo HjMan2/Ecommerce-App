bfcd21f0246bee5e718b3275c3f0c28a8071bc72
HjMan2 <79367122+HjMan2@users.noreply.github.com>
2021-07-15 23:55:28 +0430

    reformat the header

--- src/layout/Main/components/Header/Header.js
+++ src/layout/Main/components/Header/Header.js
@@ -1,92 +1,99 @@
-import React from 'react';
-import {useRouteMatch, Link} from 'react-router-dom'
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import IconButton from '@material-ui/core/IconButton';
-import Typography from '@material-ui/core/Typography';
-import MenuIcon from '@material-ui/icons/Menu';
-import {ShoppingCart} from "@material-ui/icons";
-import {createMuiTheme} from "@material-ui/core";
+import React from "react";
+import { useRouteMatch, Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Badge from "@material-ui/core/Badge";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import MenuIcon from "@material-ui/icons/Menu";
+import { ShoppingCart } from "@material-ui/icons";
+import { createMuiTheme } from "@material-ui/core";
 
 const theme = createMuiTheme({
-    direction: 'rtl',
-})
+  direction: "rtl",
+});
 const useStyles = makeStyles(() => ({
-    bgColor: {
-      backgroundColor: '#56ba94'
+  bgColor: {
+    backgroundColor: "#56ba94",
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginLeft: theme.spacing(2),
+    [theme.breakpoints.up("lg")]: {
+      display: "none",
     },
-    grow: {
-        flexGrow: 1,
+  },
+  title: {
+    fontSize: "1rem",
+    [theme.breakpoints.up("lg")]: {
+      fontSize: "1.125rem",
     },
-    menuButton: {
-        marginLeft: theme.spacing(2),
-        [theme.breakpoints.up('lg')]: {
-            display: "none"
-        }
+  },
+  links: {
+    color: "#fff",
+    textDecoration: "none",
+    "&:active,&:hover": {
+      color: "inherit",
     },
-    title: {
-        fontSize: "1rem",
-        [theme.breakpoints.up('lg')]: {
-            fontSize: "1.125rem"
-        }
-    },
-    links: {
-        color: "#fff",
-        textDecoration: "none",
-        '&:active,&:hover': {
-            color: 'inherit'
-        }
-    },
-    spacing: {
-        display: 'flex',
-        justifyContent: 'space-between',
-        width: '100px',
-        alignItems: "center",
-    },
-
+  },
+  spacing: {
+    display: "flex",
+    justifyContent: "space-between",
+    width: "100px",
+    alignItems: "center",
+  },
 }));
 
 function Header() {
-    const classes = useStyles();
-    const { path }  = useRouteMatch()
-    const formattedPath = path.slice(1)
+  const classes = useStyles();
+  const { path } = useRouteMatch();
+  const formattedPath = path.slice(1);
+  const numberOfProducts = useSelector((state) => state.products.length);
 
-    return (
-        <div className={classes.grow}>
-            <AppBar position="static" className={classes.bgColor}>
-                <Toolbar>
-                    {
-                        formattedPath.slice(0, formattedPath.indexOf('/')) === "goods" ?
-                        <IconButton
-                            edge="start"
-                            className={classes.menuButton}
-                            color="inherit"
-                            aria-label="open drawer"
-                        >
-                            <MenuIcon/>
-                        </IconButton> : null
-                    }
-                    <Typography className={classes.title} variant="h6" noWrap>
-                        <Link to="/" className={classes.links}>فروشگاه اینترنتی</Link>
-                    </Typography>
-                    <div className={classes.grow}/>
-                    <div className={classes.spacing}>
-                        <Link className={classes.links} to="/login">مدیریت</Link>
-                        <Link className={classes.links} to="/cart">
-                            <IconButton
-                                edge="start"
-                                color="inherit"
-                                aria-label="open drawer"
-                            >
-                                <ShoppingCart/>
-                            </IconButton>
-                        </Link>
-                    </div>
-                </Toolbar>
-            </AppBar>
-        </div>
-    );
+  return (
+    <div className={classes.grow}>
+      <AppBar position="static" className={classes.bgColor}>
+        <Toolbar>
+          {formattedPath.slice(0, formattedPath.indexOf("/")) === "goods" ? (
+            <IconButton
+              edge="start"
+              className={classes.menuButton}
+              color="inherit"
+              aria-label="open drawer"
+            >
+              <MenuIcon />
+            </IconButton>
+          ) : null}
+          <Typography className={classes.title} variant="h6" noWrap>
+            <Link to="/" className={classes.links}>
+              فروشگاه اینترنتی
+            </Link>
+          </Typography>
+          <div className={classes.grow} />
+          <div className={classes.spacing}>
+            <Link className={classes.links} to="/login">
+              مدیریت
+            </Link>
+            <Link className={classes.links} to="/cart">
+              <IconButton edge="start" color="inherit" aria-label="open drawer">
+                {numberOfProducts ? (
+                  <Badge color="error" badgeContent={numberOfProducts}>
+                    <ShoppingCart />
+                  </Badge>
+                ) : (
+                  <ShoppingCart />
+                )}
+              </IconButton>
+            </Link>
+          </div>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
 }
 
-export {Header}
+export { Header };

ed87e49d6dc2c6d9ba16cbc032b428af88e975e8
HjMan2 <79367122+HjMan2@users.noreply.github.com>
2021-07-15 23:49:31 +0430

    provide store for entire app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import AppRoute from "./route/App.route";
+import { Provider } from "react-redux";
 import { ToastContainer } from "react-toastify";
+import store from "./redux/store";
+import AppRoute from "./route/App.route";
 import "react-toastify/dist/ReactToastify.css";
 function App() {
   return (
-    <>
+    <Provider store={store}>
       <ToastContainer />
       <AppRoute />
-    </>
+    </Provider>
   );
 }
 
